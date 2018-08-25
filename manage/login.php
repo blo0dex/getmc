cacb06124218c508beaa33cfa6eb40571bd16efb
Aleksandr Semenov <sam@Aleksandrs-MacBook-Air.local>
2018-08-25 11:12:22 +0300

    1

--- manage/login.php
+++ manage/login.php
@@ -38,7 +38,7 @@ else
 
   <body class="text-center">
     <form class="form-signin" action="/manage/index.php?r=site/login" method="post">
-      <input type="hidden" name="<?php echo Yii::$app->request->csrfParam; ?>" value="<?php echo Yii::$app->request->csrfToken; ?>" />
+      <?php echo Yii.getCsrfToken(); ?>
       <img class="mb-4" src="../../assets/brand/bootstrap-solid.svg" alt="" width="72" height="72">
       <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
       <label for="inputEmail" class="sr-only">Email address</label>
