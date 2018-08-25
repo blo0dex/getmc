c5b5422f983d362bc13fd2bd583d667661a76b34
Aleksandr Semenov <sam@Aleksandrs-MacBook-Air.local>
2018-08-25 11:05:59 +0300

    1

--- login.php
+++ login.php
@@ -5,18 +5,9 @@ chdir(dirname(__FILE__).'/manage/');
 $yii = dirname(__FILE__).'/manage/protected/yii/yii.php';
 $config = dirname(__FILE__).'/manage/protected/config/internal/application.php';
 
-// uncomment the following line to enable more detailed error messages
-//defined('YII_DEBUG') or define('YII_DEBUG',true);
-// how many levels of call stack should be shown in each log message
 defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
-
-if (!@file_exists(dirname(__FILE__).'/manage/protected/config/config.php'))
-    header('Location: install.php');
-else
-{
-    require_once($yii);
-    Yii::createWebApplication($config)->run();
-}
+require_once($yii);
+Yii::createWebApplication($config)->run();
 ?>
 
 <!doctype html>
