68f707414f8b20169234528ad35455ee5a79793a
Aleksandr Semenov <sam@Aleksandrs-MacBook-Air.local>
2018-08-25 12:19:31 +0300

    1

--- manage/protected/controllers/UserController.php
+++ manage/protected/controllers/UserController.php
@@ -228,7 +228,8 @@ class UserController extends Controller
         require('MulticraftAPI.php');
         $api = new MulticraftAPI('http://getmc.club/manage/api.php', 'admin', 'eKfeKMUaL$WxSU');
         //$server_id = $api->createServerOn(array('daemon_id', 'no_commands', 'no_setup_script'), array(0, 0, 0));
-        $server_id = $api->createServerOn()['data']['id'];
+        $response = json_decode($api->createServerOn());
+        $server_id = $response['data']['id'];
         throw new CHttpException(404, Yii::t('mc', json_encode($server_id)));
 
         if (!Yii::app()->user->isStaff() && Yii::app()->params['hide_userlist'])
